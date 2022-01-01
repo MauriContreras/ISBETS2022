babe6c2e812ef65296be91df0feea92f2bbf22f7
porkipig <mauricg10@gmail.com>
2022-01-01 20:47:54 +0100

    prueba 3

--- src/main/java/dataAccess/ObjectdbManagerServer.java
+++ src/main/java/dataAccess/ObjectdbManagerServer.java
@@ -33,7 +33,6 @@ public class ObjectdbManagerServer extends JDialog {
 		}
 	}
 
-//wig
 	public ObjectdbManagerServer() {
 		setTitle("objectDBManagerServer: running the database server");
 		setBounds(100, 100, 486, 180);
